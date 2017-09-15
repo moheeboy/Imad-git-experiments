2bad5704f01ec16f8679c82785e4f88fbd5450e6
Mohit Nikumbh <mohitnikumbh66@gmail.com>
2017-09-15 19:03:44 +0530

    This is the second version of sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,14 @@
-class sample{
+public class sample{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		int a=4;
 		Sustem.out.println("val of a is " + a);
+		Me m = new Me();
+	}
+}
+
+class Me{
+	public Me(){
+		System.out.println("I m a fool!");
 	}
 }
